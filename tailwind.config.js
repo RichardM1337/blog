fed7e2185198e225a11cebaa558a0f2c88145e55
RichardM1337 <113366399+RichardM1337@users.noreply.github.com>
2026-04-24 11:03:02 -0400

    fixes incss
    im going to nuke the css for htis projet and put a bunch of things in public

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        atkinson: ["var(--font-atkinson)", "sans-serif"],
+        glusp: ["var(--font-glusp)", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
