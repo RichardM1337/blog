931728abec1ab3014b9cd9549fa0889dc6c0c707
RichardM1337 <113366399+RichardM1337@users.noreply.github.com>
2026-04-27 10:30:43 -0400

    mostly stripped css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        atkinson: ["var(--font-atkinson)", "sans-serif"],
         glusp: ["var(--font-glusp)", "sans-serif"],
       },
     },
